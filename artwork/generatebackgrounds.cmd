d2bc9077c96c5024219f06fe4414f03f787ff086
Alessandro Portale <aportale@trolltech.com>
2009-07-10 21:57:23 +0200

    Cleanup

--- artwork/generatebackgrounds.cmd
+++ artwork/generatebackgrounds.cmd
@@ -1,13 +1,15 @@
-for %%i in (640x480 512x384 480x360 425x320 424x320 1280x720) do call :exportsozetopng %%i
+@Echo off
+for %%i in (qt_small qt_big cc-by-nc-sa_small cc-by-nc-sa_big cc-by-nc-nd_small cc-by-nc-nd_big oldstyle) do call :exportidtopng %%i
 goto :eof
 
-:exportsozetopng
-inkscape.exe --file=qtlogosmall.svg --export-id=backgroundgroupsmall_%1 --export-id-only --export-png=qtlogosmall_%1.png --export-background-opacity=0
-pngout qtlogosmall_%1.png
-inkscape.exe --file=cclogo.svg --export-id=backgroundgroupsmall_%1 --export-id-only --export-png=cclogosmall_%1.png --export-background-opacity=0
-pngout cclogosmall_%1.png
-inkscape.exe --file=cclogo.svg --export-id=backgroundgroupbig_%1 --export-id-only --export-png=cclogobig_%1.png --export-background-opacity=0
-pngout cclogobig_%1.png
-inkscape.exe --file=oldstylegradient.svg --export-id=gradient_%1 --export-id-only --export-png=oldstylegradient_%1.png --export-background-opacity=0
-pngout oldstylegradient_%1.png
+:exportidtopng
+for %%j in (1280x720 640x480 480x360 424x320) do call :exportidwithsizetopng %1 %%j
+goto :eof
+
+:exportidwithsizetopng
+SET EXPORT_ID=%1
+SET EXPORT_ID=%EXPORT_ID%_%2
+inkscape.exe --file=qtlogosmall.svg --export-id=%EXPORT_ID% --export-id-only --export-png=%EXPORT_ID%.png --export-background-opacity=0
+pngout %EXPORT_ID%.png /c6
+optipng -quiet -o7 -zw32k %EXPORT_ID%.png
 goto :eof
