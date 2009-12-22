81a73144643486a745a2cd771e83249196869b6a
Alessandro Portale <alessandro@casaportale.de>
2009-12-22 18:58:36 +0100

    Added Symbian clips

--- concepts/qtorials.mm
+++ concepts/qtorials.mm
@@ -1,6 +1,6 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node COLOR="#000000" CREATED="1236466048994" ID="Freemind_Link_788798378" MODIFIED="1247311995799" TEXT="Qtorials">
+<node COLOR="#000000" CREATED="1236466048994" ID="Freemind_Link_788798378" MODIFIED="1261488502255" TEXT="Qtorials">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -19,7 +19,7 @@
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
 <attribute_layout VALUE_WIDTH="150"/>
 <attribute NAME="clip_tags" VALUE="qt, tutorial, qtorial, open source, software, development, nokia"/>
-<node COLOR="#0033ff" CREATED="1237319164140" ID="ID_1094202060" MODIFIED="1247312080161" POSITION="right" TEXT="Qt SDK installation">
+<node COLOR="#0033ff" CREATED="1237319164140" ID="ID_1094202060" MODIFIED="1261491150447" POSITION="right" TEXT="Qt SDK installation">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -35,15 +35,13 @@
 <font NAME="SansSerif" SIZE="18"/>
 <attribute_layout NAME_WIDTH="119" VALUE_WIDTH="164"/>
 <attribute NAME="clip_tags" VALUE="installation, getting started"/>
-<node COLOR="#00b439" CREATED="1237319427687" ID="ID_1016574805" LINK="../screencasts/sdkinstallation/windows.avs" MODIFIED="1247311348949" TEXT="Windows">
+<node COLOR="#00b439" CREATED="1237319427687" ID="ID_1016574805" LINK="../screencasts/sdkinstallation/windows.avs" MODIFIED="1261438169068" TEXT="Windows">
 <richcontent TYPE="NOTE"><html>
   <head>
     
   </head>
   <body>
-    <p>
-      How to download and install Qt SDK on Windows.
-    </p>
+    How to download and install Qt SDK on Windows.
   </body>
 </html></richcontent>
 <edge STYLE="bezier" WIDTH="thin"/>
@@ -53,7 +51,7 @@
 <attribute NAME="clip_length" VALUE="3:18"/>
 <attribute NAME="youtube_id" VALUE="zf9XYs902Mw"/>
 </node>
-<node COLOR="#00b439" CREATED="1237319440468" ID="ID_1734709603" MODIFIED="1239027576015" TEXT="OSX">
+<node COLOR="#00b439" CREATED="1237319440468" ID="ID_1734709603" MODIFIED="1261437178823" TEXT="OSX">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -69,7 +67,7 @@
 <attribute_layout NAME_WIDTH="91" VALUE_WIDTH="64"/>
 <attribute NAME="clip_tags" VALUE="osx, mac"/>
 </node>
-<node COLOR="#00b439" CREATED="1237319443859" ID="ID_1428500051" MODIFIED="1247310834956" TEXT="Linux">
+<node COLOR="#00b439" CREATED="1237319443859" ID="ID_1428500051" MODIFIED="1261491141097" TEXT="Linux">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -79,14 +77,15 @@
       Installation of the prerequisite packages on (K)ubuntu or other debian based systems. How to download the Qt SDK, setting the file mode and installing the Qt SDK.
     </p>
   </body>
-</html></richcontent>
+</html>
+</richcontent>
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 <attribute_layout NAME_WIDTH="90" VALUE_WIDTH="47"/>
 <attribute NAME="clip_tags" VALUE="linux"/>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1236466098815" ID="_" MODIFIED="1247312078975" POSITION="right" TEXT="Widgets crash course">
+<node COLOR="#0033ff" CREATED="1236466098815" FOLDED="true" ID="_" MODIFIED="1261491149022" POSITION="right" TEXT="Widgets crash course">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -102,7 +101,7 @@
 <font NAME="SansSerif" SIZE="18"/>
 <attribute_layout VALUE_WIDTH="141"/>
 <attribute NAME="clip_tags" VALUE="widget, basics"/>
-<node COLOR="#00b439" CREATED="1238281745448" ID="ID_1889645545" LINK="../screencasts/widgetcrashcourse/showingwidgets.avs" MODIFIED="1247311786798" TEXT="Showing widgets">
+<node COLOR="#00b439" CREATED="1238281745448" ID="ID_1889645545" LINK="../screencasts/widgetcrashcourse/showingwidgets.avs" MODIFIED="1261437195527" TEXT="Showing widgets">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -118,14 +117,14 @@
 <attribute_layout NAME_WIDTH="99" VALUE_WIDTH="172"/>
 <attribute NAME="clip_tags" VALUE="instantiating, showing"/>
 </node>
-<node COLOR="#00b439" CREATED="1238281873553" ID="ID_1426700040" LINK="../screencasts/widgetcrashcourse/composingdialog.avs" MODIFIED="1238446603685" TEXT="Composing a dialog">
+<node COLOR="#00b439" CREATED="1238281873553" ID="ID_1426700040" LINK="../screencasts/widgetcrashcourse/composingdialog.avs" MODIFIED="1261439633671" TEXT="Composing a dialog">
 <richcontent TYPE="NOTE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      How to put the button and the slider into the dialog. A first&#160;contact with parent-child relationships in Qt.
+      How to put the button and the slider into the dialog. A first contact with parent-child relationships in Qt.
     </p>
   </body>
 </html></richcontent>
@@ -134,7 +133,7 @@
 <attribute_layout NAME_WIDTH="103" VALUE_WIDTH="186"/>
 <attribute NAME="clip_tags" VALUE="parent, child, composing"/>
 </node>
-<node COLOR="#00b439" CREATED="1238281893288" ID="ID_65287530" LINK="../screencasts/widgetcrashcourse/layingoutwidgets.avs" MODIFIED="1238935272825" TEXT="Laying out widgets">
+<node COLOR="#00b439" CREATED="1238281893288" ID="ID_65287530" LINK="../screencasts/widgetcrashcourse/layingoutwidgets.avs" MODIFIED="1261439635247" TEXT="Laying out widgets">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -150,7 +149,7 @@
 <attribute_layout NAME_WIDTH="98" VALUE_WIDTH="156"/>
 <attribute NAME="clip_tags" VALUE="layout"/>
 </node>
-<node COLOR="#00b439" CREATED="1238281917035" ID="ID_1327950026" LINK="../screencasts/widgetcrashcourse/qtdesigner.avs" MODIFIED="1247312039766" TEXT="Using QtDesigner">
+<node COLOR="#00b439" CREATED="1238281917035" ID="ID_1327950026" LINK="../screencasts/widgetcrashcourse/qtdesigner.avs" MODIFIED="1261437177441" TEXT="Using QtDesigner">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -160,15 +159,14 @@
       Using the wizard to create a gui project. It is much easier to create the dialog in the QtDesigner.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 <attribute_layout VALUE_WIDTH="125"/>
 <attribute NAME="clip_tags" VALUE="qt designer, dialog"/>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1238616490681" FOLDED="true" ID="ID_939887336" MODIFIED="1247312049814" POSITION="right" TEXT="Deploying Qt apps">
+<node COLOR="#0033ff" CREATED="1238616490681" ID="ID_939887336" MODIFIED="1261491168184" POSITION="right" TEXT="Deploying Qt apps">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -184,23 +182,24 @@
 <font NAME="SansSerif" SIZE="18"/>
 <attribute_layout VALUE_WIDTH="162"/>
 <attribute NAME="clip_tags" VALUE="deploy, ship"/>
-<node COLOR="#00b439" CREATED="1238616723436" ID="ID_535173612" MODIFIED="1238620769002" TEXT="Windows (easy)">
+<node COLOR="#00b439" CREATED="1238616723436" ID="ID_535173612" MODIFIED="1261491321622" TEXT="Windows (easy)">
 <richcontent TYPE="NOTE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      The needed files.&#160;The locations of&#160;Qt plugins. Redistributing compiler specific runtimes. Creation of an installer package.
+      The needed files. The locations of Qt plugins. Redistributing compiler specific runtimes. Creation of an installer package.
     </p>
   </body>
-</html></richcontent>
+</html>
+</richcontent>
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 <attribute_layout VALUE_WIDTH="151"/>
 <attribute NAME="clip_tags" VALUE="windows, compact, static"/>
 </node>
-<node COLOR="#00b439" CREATED="1238616744846" ID="ID_728150918" MODIFIED="1238928705580" TEXT="Windows (compact)">
+<node COLOR="#00b439" CREATED="1238616744846" ID="ID_728150918" MODIFIED="1261438285909" TEXT="Windows (compact)">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -216,7 +215,7 @@
 <attribute_layout VALUE_WIDTH="205"/>
 <attribute NAME="clip_tags" VALUE="windows, compact, static, installer"/>
 </node>
-<node COLOR="#00b439" CREATED="1238616757494" ID="ID_1006485996" MODIFIED="1238620698568" TEXT="OSX (easy)">
+<node COLOR="#00b439" CREATED="1238616757494" ID="ID_1006485996" MODIFIED="1261438287411" TEXT="OSX (easy)">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -232,7 +231,7 @@
 <attribute_layout VALUE_WIDTH="154"/>
 <attribute NAME="clip_tags" VALUE="osx, mac, easy, dmg, disk image"/>
 </node>
-<node COLOR="#00b439" CREATED="1238616765187" ID="ID_662328517" MODIFIED="1238620653889" TEXT="OSX (compact)">
+<node COLOR="#00b439" CREATED="1238616765187" ID="ID_662328517" MODIFIED="1261438360483" TEXT="OSX (compact)">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -249,11 +248,87 @@
 <attribute NAME="clip_tags" VALUE="osx, mac, compact, static, dmg disk image"/>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1239254106887" ID="ID_681246977" MODIFIED="1239254131328" POSITION="right" TEXT="Internationalization">
+<node COLOR="#0033ff" CREATED="1237319164140" ID="ID_840970220" MODIFIED="1261491421528" POSITION="right" TEXT="Qt for Symbian">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      How to get the Qt SDK running on your system. Also taking a first look at the QtDemo and QtCreator.
+    </p>
+  </body>
+</html>
+</richcontent>
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
+<attribute_layout NAME_WIDTH="119" VALUE_WIDTH="164"/>
+<attribute NAME="clip_tags" VALUE="Symbian, s60"/>
+<node COLOR="#00b439" CREATED="1237319427687" ID="ID_1533522103" LINK="../screencasts/sdkinstallation/windows.avs" MODIFIED="1261491763427" TEXT="Setting up a Symbian/S60 development environment ">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <!--
+        p { margin-top: 0 }
+        body { font-size: 12pt; font-family: SansSerif }
+      -->
+    
+
+    <p>
+      How to set up a basic&#160;Symbian/S60 development environment on Windows.
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<attribute_layout NAME_WIDTH="94" VALUE_WIDTH="222"/>
+<attribute NAME="clip_tags" VALUE="development environment, setup, s60 sdk, carbide.c++, active perl, application trk"/>
+<attribute NAME="clip_length" VALUE="5:20"/>
+<attribute NAME="youtube_id" VALUE="Sh58z-goVn4"/>
 </node>
-<node COLOR="#0033ff" CREATED="1238930730887" FOLDED="true" ID="ID_1017245630" MODIFIED="1247312043894" POSITION="right" TEXT="Qt tidbits">
+<node COLOR="#00b439" CREATED="1237319440468" ID="ID_990970024" MODIFIED="1261491678917" TEXT="Setting up Qt for Symbian">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      How to set up Qt for Symbian on top of an existing Symbian/S60 development environment.
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<attribute_layout NAME_WIDTH="91" VALUE_WIDTH="175"/>
+<attribute NAME="clip_tags" VALUE="Qt Sdk, setup"/>
+<attribute NAME="clip_length" VALUE="2:52"/>
+<attribute NAME="youtube_id" VALUE="bGJOQTkdttM"/>
+</node>
+<node COLOR="#00b439" CREATED="1237319443859" ID="ID_856080445" MODIFIED="1261491840571" TEXT="Developing in Qt Creator">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      How to create a small Qt-based Symbian application in Qt Creator - running it on Windows, compiling for the phone and running on the phone.
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<attribute_layout NAME_WIDTH="90" VALUE_WIDTH="235"/>
+<attribute NAME="clip_tags" VALUE="Qt Creator, development"/>
+<attribute NAME="clip_length" VALUE="5:02"/>
+<attribute NAME="youtube_id" VALUE="Rb43gnZI1A0"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1238930730887" FOLDED="true" ID="ID_1017245630" MODIFIED="1261488510204" POSITION="right" TEXT="Qt tidbits">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -283,14 +358,6 @@
 <font NAME="SansSerif" SIZE="16"/>
 <attribute_layout VALUE_WIDTH="129"/>
 <attribute NAME="clip_tags" VALUE="signal mapper"/>
-</node>
-</node>
-<node COLOR="#0033ff" CREATED="1248036909043" FOLDED="true" ID="ID_1353909696" LINK="http://creativecommons.org/licenses/by-nc-sa/3.0/" MODIFIED="1248037007287" POSITION="left" TEXT="License">
-<edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1248036914273" ID="ID_1496406040" LINK="http://creativecommons.org/licenses/by-nc-sa/3.0/" MODIFIED="1248036957085" TEXT="This work is licensed under the Creative Commons Attribution-Noncommercial-Share Alike 3.0 Unported License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="SansSerif" SIZE="16"/>
 </node>
 </node>
 </node>
