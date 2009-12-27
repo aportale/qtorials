b87b8adbe0845ec74078fec89bf552f7eec29604
Alessandro Portale <alessandro@casaportale.de>
2009-12-27 15:09:39 +0100

    Non-invasive, gracefully-degrading Javascript bling.
    That's quite a mouth full of buzzwords.

---
+++ overview/html/script.js
@@ -0,0 +1,45 @@
+var youtubePlayer;
+$(document).ready(function(){
+    $("#qtorials").after('<div style="display:none" id="youtubeplayer"></div>');
+    youtubePlayer = $("#youtubeplayer");
+    var clipLis = $("#qtorials > li > ul > li");
+    $(clipLis).each(function(i){
+        addViewLinks(this, i);
+    });
+});
+
+function addViewLinks(clipLi, i)
+{
+    var firstAnchor = $(clipLi).find("a:first");
+    $(firstAnchor).removeAttr("href");
+    var clipLength = $(clipLi).find(".cliplength:first");
+    var clipId = qtorialsData["clips"][i]["youtube_id"];
+    var clipWidth = qtorialsData["clips"][i]["clip_width"];
+    var clipHeight = qtorialsData["clips"][i]["clip_height"] + 25;
+    var youTubeHref = "http://www.youtube.com/watch?v=" + clipId;
+    $(clipLength).before('<li><a href="#youtubeplayer">View Qtorial</a></li>');
+    $(clipLi).find("a:last").fancybox({
+        'callbackOnStart' : function()
+            {
+                var objectString =
+                    '<object width="' + clipWidth + '" height="' + clipHeight + '">'
+                    + '<param name="movie" value="http://www.youtube.com/v/' + clipId + '&hl=en_GB&fs=1&"></param>'
+                    + '<param name="allowFullScreen" value="true"></param>'
+                    + '<param name="allowscriptaccess" value="always"></param>'
+                    + '<embed src="http://www.youtube.com/v/' + clipId + '&hl=en_GB&fs=1&autoplay=1" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true"'
+                    + 'width="' + clipWidth + '" height="' + clipHeight + '"></embed></object>';
+                $(youtubePlayer).width(clipWidth);
+                $(youtubePlayer).height(clipHeight);
+                $(youtubePlayer).append(objectString);
+            },
+        'callbackOnClose' : function()
+            {
+                $("#youtubeplayer").empty();
+            },
+        'frameWidth' : clipWidth,
+        'frameHeight' : clipHeight,
+        'overlayOpacity' : 0.6,
+        'centerOnScroll' : false
+    });
+    $(clipLength).before('<li><a href="' + youTubeHref + '">YouTube Page</a></li>');
+}
