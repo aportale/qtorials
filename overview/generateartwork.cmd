a437e3246fa80150098f7f4e53f495f9b797a302
Alessandro Portale <alessandro@casaportale.de>
2009-12-28 14:25:29 +0100

    Automating export of css images wit ha script

---
+++ overview/generateartwork.cmd
@@ -0,0 +1,22 @@
+@echo off
+rem This work is licensed under the Creative Commons
+rem Attribution-Noncommercial-Share Alike 3.0 Unported
+rem License. To view a copy of this license, visit
+rem http://creativecommons.org/licenses/by-nc-sa/3.0/
+rem or send a letter to Creative Commons,
+rem 171 Second Street, Suite 300, San Francisco,
+rem California, 94105, USA.
+
+set SCRIPTDIR=%~d0%~p0
+set OUTPUTDIR=%SCRIPTDIR%html\
+
+for %%i in (bgtop bgmiddle bgbottom) do call :exportidtopng %%i
+goto :eof
+
+:exportidtopng
+SET EXPORT_ID=%1
+SET PNGFILE=%OUTPUTDIR%%EXPORT_ID%.png
+inkscape.exe --file=%SCRIPTDIR%artwork.svg --export-id=%EXPORT_ID% --export-png=%PNGFILE%
+pngout %PNGFILE% /c6
+optipng -quiet -o7 -zw32k %PNGFILE%
+goto :eof
