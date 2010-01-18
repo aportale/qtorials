89d24361792e40b27f2d991aa9ce9401ca1e5b7f
Alessandro Portale <alessandro@casaportale.de>
2010-01-18 16:18:38 +0100

    x264 for encoding

--- tools/bin/encodeqtorial_mp4.cmd
+++ tools/bin/encodeqtorial_mp4.cmd
@@ -12,5 +12,8 @@ set SCREENCASTSDIR=%SCRIPTDIR%..\..\screencasts
 set SCREENCASTAVS=%SCREENCASTSDIR%\%1.avs
 set STAGEDIR=%SCRIPTDIR%..\..\..\qtorials_stage
 set SCREENCASTMP4=%STAGEDIR%\%1.mp4
+set SCREENCASTMP4TEMP=%STAGEDIR%\%1_temp.mp4
 
-ffmpeg -threads 0 -y -i "%SCREENCASTAVS%" -vb 2500000 -vcodec libx264 "%SCREENCASTMP4%"
+x264 -o "%SCREENCASTMP4TEMP%" --threads 1 --crf 15 "%SCREENCASTAVS%"
+ffmpeg -y -i "%SCREENCASTMP4TEMP%" -vcodec copy -i "%SCREENCASTAVS%" -acodec libfaac -ac 1 -ar 44100 -ab 64k "%SCREENCASTMP4%"
+del "%SCREENCASTMP4TEMP%"
